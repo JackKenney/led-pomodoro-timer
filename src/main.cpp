2d13a11e0fce88d40b11ee31e3890f4e17a2080d
Jack Kenney <jack@kenney.dev>
2021-12-11 20:46:20 -0500

    random starter code

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,23 @@
 
 const uint8_t NUM_LEDS = 5;
 CRGB leds[NUM_LEDS];
-CRGB color = CRGB(128, 0, 128);
+Button button = Button(5);
 
-void setup()
+bool lightsOn = true;
+
+void setLEDs(uint8_t r, uint8_t g, uint8_t b)
 {
+  CRGB color = CRGB(r, g, b);
   for (int i = 0; i < NUM_LEDS; i++)
   {
     leds[i] = color;
   }
+}
 
+void setup()
+{
+  setLEDs(10, 0, 10);
+  // setLEDs(0, 0, 0);
   FastLED.addLeds<APA102, MOSI, SCK, BGR>(leds, NUM_LEDS);
   // FastLED.setDither(0);
   // FastLED.clear(true);
@@ -26,4 +34,19 @@ void setup()
 
 void loop()
 {
+  // if (button.pressed())
+  // {
+  //   if (lightsOn)
+  //   {
+  //     setLEDs(0, 0, 0);
+  //     lightsOn = false;
+  //   }
+  //   else
+  //   {
+  //     setLEDs(128, 0, 128);
+  //     lightsOn = true;
+  //   }
+  //   FastLED.addLeds<APA102, MOSI, SCK, BGR>(leds, NUM_LEDS);
+  //   FastLED.show();
+  // }
 }
