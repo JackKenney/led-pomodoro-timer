12b804529865f31cbc50a9faeca9633954637fc2
Jack Kenney <jack@kenney.dev>
2021-12-11 21:26:03 -0500

    random colors + longpress

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
-#include <Button.h>
+// #include <Button.h>
 #include <EEPROM.h>
 #include <FastLED.h>
 
 #define SEC_IN_MS(N) (N * 1000l)
 #define MIN_IN_MS(N) (N * 60000l)
 
+const uint8_t BUTTON_PIN = 5;
 const uint8_t NUM_LEDS = 5;
 CRGB leds[NUM_LEDS];
-Button button = Button(5);
+const uint8_t MAX_BRIGHTNESS = 128;
+// Button button = Button(5);
 
 bool lightsOn = true;
 
@@ -30,23 +32,42 @@ void setup()
   // FastLED.clear(true);
 
   FastLED.show();
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+int lastState = HIGH;
+int currentState;
+unsigned long pressedTime = 0;
+unsigned long releasedTime = 0;
+
+bool isLongPress()
+{
+  return releasedTime - pressedTime >= SEC_IN_MS(0.5);
 }
 
 void loop()
 {
-  // if (button.pressed())
-  // {
-  //   if (lightsOn)
-  //   {
-  //     setLEDs(0, 0, 0);
-  //     lightsOn = false;
-  //   }
-  //   else
-  //   {
-  //     setLEDs(128, 0, 128);
-  //     lightsOn = true;
-  //   }
-  //   FastLED.addLeds<APA102, MOSI, SCK, BGR>(leds, NUM_LEDS);
-  //   FastLED.show();
-  // }
+  currentState = digitalRead(BUTTON_PIN);
+  if (lastState == HIGH && currentState == LOW) // pressed
+  {
+    pressedTime = millis();
+  }
+  else if (lastState == LOW && currentState == HIGH) // released
+  {
+    releasedTime = millis();
+
+    if (isLongPress())
+    {
+      setLEDs(0, 0, 0);
+      FastLED.addLeds<APA102, MOSI, SCK, BGR>(leds, NUM_LEDS);
+    }
+    else
+    {
+      setLEDs(rand() % MAX_BRIGHTNESS, rand() % MAX_BRIGHTNESS, rand() % MAX_BRIGHTNESS);
+      FastLED.addLeds<APA102, MOSI, SCK, BGR>(leds, NUM_LEDS);
+    }
+  }
+  FastLED.show();
+  lastState = currentState;
 }
