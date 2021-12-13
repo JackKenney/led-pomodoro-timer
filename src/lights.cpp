5f85939cb4897fcb8aca886f8b46bd0f0630cc4b
Jack Kenney <jack@kenney.dev>
2021-12-12 21:53:18 -0500

    overload setLED functions

--- src/lights.cpp
+++ src/lights.cpp
@@ -5,12 +5,23 @@ CRGB leds[NUM_LEDS];
 void setLED(uint8_t idx, uint8_t r, uint8_t g, uint8_t b)
 {
     CRGB color = CRGB(r, g, b);
+    setLED(idx, color);
+}
+
+void setLED(uint8_t idx, CRGB color)
+{
     leds[idx] = color;
     FastLED.addLeds<APA102, MOSI, SCK, BGR>(leds, NUM_LEDS);
 }
 
 void setAllLEDs(uint8_t r, uint8_t g, uint8_t b)
 {
+    setAllLEDs(CRGB(r,g,b));
+}
+
+
+void setAllLEDs(CRGB color)
+{
     for (int i = 0; i < NUM_LEDS; i++)
-        setLED(i, r, g, b);
+        setLED(i, color);
 }
--- src/lights.hpp
+++ src/lights.hpp
@@ -3,4 +3,7 @@
 #include "consts.hpp"
 
 void setLED(uint8_t idx, uint8_t r, uint8_t g, uint8_t b);
+void setLED(uint8_t idx, CRGB color);
+
 void setAllLEDs(uint8_t r, uint8_t g, uint8_t b);
+void setAllLEDs(CRGB color);
